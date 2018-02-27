a3738fbdc6e20a3177dab3b00a7bfc79c6fd34be
ijon1969 <yoichi.omori@gmail.com>
2018-02-27 22:34:20 +0900

    Arrange direcories

--- jp.ac.kyushu_u.csce.modeltool.dictionary/src/jp/ac/kyushu_u/csce/modeltool/dictionary/dict/DictionaryInfo.java
+++
@@ -1,9 +0,0 @@
-package jp.ac.kyushu_u.csce.modeltool.dictionary.dict;
-
-/**
- * 辞書情報を格納するインターフェース
- */
-public interface DictionaryInfo {
-
-	public DictionaryInfo deepCopy();
-}
--- jp.ac.kyushu_u.csce.modeltool.dictionary/src/jp/ac/kyushu_u/csce/modeltool/dictionary/dict/ITableTabFileLoader.java
+++
@@ -1,6 +0,0 @@
-package jp.ac.kyushu_u.csce.modeltool.dictionary.dict;
-
-public interface ITableTabFileLoader {
-
-	public void afterLoadFile(TableTab _tab);
-}
